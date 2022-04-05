cf69007654711c7b692bfc430760a803bce5f1c7
ArturBekhDEV <artbekhdev@gmail.com>
2022-04-05 14:33:24 +0300

    Updating. v 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header-component/header.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
